6f734ed1fa6ac82d95eccdcb951262cdaae3f14a
dex0501 <parmacmarin@gmail.com>
2023-03-19 23:44:22 +0100

    Remove .plugins

--- .plugins/obsidian-calendar-plugin-master/src/ui/__mocks__/obsidian.ts
+++
@@ -1,7 +0,0 @@
-export class TFile {}
-export class PluginSettingTab {}
-export class Modal {}
-export class Notice {}
-export function normalizePath(): string {
-  return "";
-}
--- .plugins/obsidian-calendar-plugin-master/src/ui/sources/index.ts
+++
@@ -1,4 +0,0 @@
-export { streakSource } from "./streak";
-export { customTagsSource } from "./tags";
-export { tasksSource } from "./tasks";
-export { wordCountSource } from "./wordCount";
